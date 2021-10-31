350441085375284ad1b9f33bf93377d2c173ca3e
rkarma69 <rkarma69@hotmail.com>
2021-10-31 16:15:48 +0900

    9th

--- explore_page.py
+++ explore_page.py
@@ -10,8 +10,8 @@ import pandas as pd
 @st.cache
 def load_data():
     names = ['Temperature', 'Humidity', 'AirPressure', 'Class']
-    #connection = mysql.connector.connect(host='192.168.219.102',user='iotuser',password="iot12345", database='iot')
-    connection = mysql.connector.connect(host='112.157.171.74',user='iotuser',password="iot12345", database='iot')
+    connection = mysql.connector.connect(host='192.168.219.102',user='iotuser',password="iot12345", database='iot')
+    #connection = mysql.connector.connect(host='112.157.171.74',user='iotuser',password="iot12345", database='iot')
 
     mycursor = connection.cursor()
     
